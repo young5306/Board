3c4be76528947de8a1f0bc021d461574623f018f
young5306 <harrysunae@naver.com>
2025-01-06 22:13:55 +0900

    sp

--- springboot-developer/src/main/java/me/young/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package me.young.springbootdeveloper.repository;
-
-import me.young.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
